81157290e166f4bcb7f6e821f8abcbd4b20fd5e9
WhyMyMilkChunky <baconpsychedelic13@gmail.com>
2023-10-14 13:27:43 -0400

    Started speed

---
+++ Assets/Scenes/Scripts/SpeedBoost.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<Player>().speed *= 2;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StartCoroutine(DelayedAction());
+    }
+    IEnumerator DelayedAction()
+    {
+        yield return new WaitForSeconds(2f);
+
+        GetComponent<Player>().speed /= 2;
+    }
+}
