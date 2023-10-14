aa4ed8d36070fc92f730df289c569d2f5b8b41b6
WhyMyMilkChunky <baconpsychedelic13@gmail.com>
2023-10-14 11:58:35 -0400

    movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float rotationSpeed = 30.0f; 
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDir = 0.0f;
+        float yDir = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            yDir = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            yDir = -1.0f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            xDir = -1.0f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xDir = 1.0f;
+        }
+
+        rb.velocity = new Vector2(xDir, yDir) * speed;
+
+        
+        if (Input.GetKey(KeyCode.E))
+        {
+            
+            transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
+        }
+        
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
